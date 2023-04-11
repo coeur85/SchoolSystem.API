7944daaa3281e014011b42493c281c63f0301996
ahmed magdi <ahmed.magdi@bgomla.com>
2023-04-12 01:59:52 +0200

    impelmnted manula validation

--- SchoolSystem.API/Domain/Queries/Students/GetOneStudentQueryHandler.cs
+++ SchoolSystem.API/Domain/Queries/Students/GetOneStudentQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 using SchoolSystem.API.Domain.Communication.Reponse;
 using SchoolSystem.API.Domain.Models.Students;
 using SchoolSystem.API.Domain.Models.Students.Exceptions;
@@ -9,16 +10,24 @@ namespace SchoolSystem.API.Domain.Queries.Students
     public class GetOneStudentQueryHandler : IRequestHandler<GetOneStudentQuery, SchoolResponse<Student>>
     {
         private readonly IStudentsRepository studentsRepository;
+        private readonly IValidator<GetOneStudentQuery> validator;
 
-        public GetOneStudentQueryHandler(IStudentsRepository studentsRepository)
+        public GetOneStudentQueryHandler(IStudentsRepository studentsRepository,
+            IValidator<GetOneStudentQuery> validator)
         {
             this.studentsRepository = studentsRepository;
+            this.validator = validator;
         }
         public async Task<SchoolResponse<Student>> Handle(GetOneStudentQuery request, CancellationToken cancellationToken)
         {
-           if (request.Id <= 0) { 
+            var result = this.validator.Validate(request);
+
+           if (! result.IsValid) { 
                 ErrorResponse<Student> errorResponse =new ErrorResponse<Student>();
-                errorResponse.AddError(new InvalidStudentIdException(request.Id));
+                foreach (var ex in result.Errors)
+                {
+                    errorResponse.AddError(ex.PropertyName, ex.ErrorMessage);
+                }
                 return errorResponse;
             }
             Student student = await this.studentsRepository.SelectOneAsync(request.Id);
---
+++ SchoolSystem.API/Domain/Queries/Students/GetOneStudentQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace SchoolSystem.API.Domain.Queries.Students
+{
+    public class GetOneStudentQueryValidator : AbstractValidator<GetOneStudentQuery>
+    {
+        public GetOneStudentQueryValidator()
+        {
+            RuleFor(x => x.Id).GreaterThan(0);
+        }
+    }
+}
