46f5b34cfdb68b4c3e32854b054ea5ff4348fb00
ahmed magdi <ahmed.magdi@bgomla.com>
2023-04-11 16:56:31 +0200

    Get Oner student command and handler

---
+++ SchoolSystem.API/Domain/Queries/Students/GetOneStudentCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using SchoolSystem.API.Domain.Models;
+
+namespace SchoolSystem.API.Domain.Queries.Students
+{
+    public class GetOneStudentCommand : IRequest<Student>
+    {
+        public int Id { get; init; }
+        public GetOneStudentCommand(int Id)
+        {
+            this.Id = Id;
+        }
+    }
+}
---
+++ SchoolSystem.API/Domain/Queries/Students/IRepositoryMangerHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using SchoolSystem.API.Domain.Models;
+using SchoolSystem.API.Domain.Repositories;
+
+namespace SchoolSystem.API.Domain.Queries.Students
+{
+    public class IRepositoryMangerHandler : IRequestHandler<GetOneStudentCommand, Student>
+    {
+        private readonly StudentsRepository studentsRepository;
+
+        public IRepositoryMangerHandler(StudentsRepository studentsRepository)
+        {
+            this.studentsRepository = studentsRepository;
+        }
+        public async Task<Student> Handle(GetOneStudentCommand request, CancellationToken cancellationToken)
+        {
+           if (request.Id <= 0) { throw new Exception("invalid student id"); }
+            Student student = await this.studentsRepository.SelectOneAsync(request.Id);
+            return student;
+        }
+    }
+}
