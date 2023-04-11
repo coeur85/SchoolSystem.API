e071bcd8549022e5147241061ce8aa292d1ff60b
ahmed magdi <ahmed.magdi@bgomla.com>
2023-04-11 16:45:49 +0200

    changed updated command Logic

--- SchoolSystem.API/Domain/Commands/Students/UpdateStudentCommandHandler.cs
+++ SchoolSystem.API/Domain/Commands/Students/UpdateStudentCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using SchoolSystem.API.Domain.Models;
 using SchoolSystem.API.Domain.Repositories;
+using System.Data.SqlTypes;
 
 namespace SchoolSystem.API.Domain.Commands.Students
 {
@@ -14,9 +15,14 @@ namespace SchoolSystem.API.Domain.Commands.Students
         }
         public async Task<Student> Handle(UpdateStudentCommand request, CancellationToken cancellationToken)
         {
-            Student updateStudent = new Student() { Id = request.Id , Name = request.Name };
-            await  studentsRepository.UpdateAsync(updateStudent);
-            return updateStudent;
+            Student studentToBeUpdated = new Student() { Id = request.Id , Name = request.Name };
+            Student dbStudent = await this.studentsRepository.SelectOneAsync(request.Id);
+            if (dbStudent == null) { throw new Exception("Student Not Found"); }
+            if(dbStudent.Name == studentToBeUpdated.Name) { throw new Exception($" student name has not been changed"); }
+
+
+            await  studentsRepository.UpdateAsync(studentToBeUpdated);
+            return studentToBeUpdated;
         }
     }
 }
