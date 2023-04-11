78f219fde7e56fcdf9683b7c028e85be5d8128f7
ahmed magdi <ahmed.magdi@bgomla.com>
2023-04-11 16:42:33 +0200

    RemoveStudentCommand & handler

---
+++ SchoolSystem.API/Domain/Commands/Students/RemoveStudentCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using SchoolSystem.API.Domain.Models;
+
+namespace SchoolSystem.API.Domain.Commands.Students
+{
+    public class RemoveStudentCommand : IRequest<Student>
+    {
+        public int Id { get; init; }
+        public string Name { get; init; }
+
+        public RemoveStudentCommand(int id, string name)
+        {
+                this.Id = id;this.Name = name;
+        }
+    }
+}
---
+++ SchoolSystem.API/Domain/Commands/Students/RemoveStudentCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using SchoolSystem.API.Domain.Models;
+using SchoolSystem.API.Domain.Repositories;
+
+namespace SchoolSystem.API.Domain.Commands.Students
+{
+    public class RemoveStudentCommandHandler : IRequestHandler<RemoveStudentCommand, Student>
+    {
+        private readonly IStudentsRepository studentsRepository;
+
+        public RemoveStudentCommandHandler(IStudentsRepository studentsRepository)
+        {
+            this.studentsRepository = studentsRepository;
+        }
+        public async Task<Student> Handle(RemoveStudentCommand request, CancellationToken cancellationToken)
+        {
+            Student deletedStudent = await this.studentsRepository.SelectOneAsync(request.Id);
+            if (deletedStudent == null) { throw new Exception("Student Not found"); }
+
+            await this.studentsRepository.DeleteAsync(deletedStudent.Id);
+
+            return deletedStudent;
+        }
+    }
+}
