0f3344a5d3b8c8f053943aecf77e3a57ec22a283
ahmed magdi <ahmed.magdi@bgomla.com>
2023-04-11 16:05:11 +0200

    main repo methods

---
+++ SchoolSystem.API/Domain/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolSystem.API.Domain.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        ValueTask InsertAsync(T entity);
+        ValueTask<List<T>> SelectAllAsync();
+        ValueTask<T?> SelectOneAsync(int id);
+        void UpdateAsync(T entity);
+    }
+}
---
+++ SchoolSystem.API/Domain/Repositories/Repository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SchoolSystem.API.Domain.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly SchoolDbContext context;
+        public Repository(SchoolDbContext context)
+        => this.context = context;
+
+
+        public virtual async ValueTask InsertAsync(T entity)
+            => await this.context.Set<T>().AddAsync(entity);
+        public virtual void UpdateAsync(T entity)
+            => this.context.Set<T>().Update(entity);
+        public virtual async ValueTask<T?> SelectOneAsync(int id)
+            => await this.context.Set<T>().FindAsync(id);
+
+        public virtual async ValueTask<List<T>> SelectAllAsync()
+            => await this.context.Set<T>().ToListAsync();
+    }
+}
